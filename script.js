461beff928c473aa96cc94a2beee074feb46488a
Affan-Rafique <affanrafique9@gmail.com>
2025-09-17 21:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
+   let table = document.getElementById("sampleTable");
+
+  // Insert row at top (index 0)
+  let newRow = table.insertRow(0);
+
+  // Insert two cells
+  let cell1 = newRow.insertCell(0);
+  let cell2 = newRow.insertCell(1);
+
+  // Add text to cells
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
   
 }
